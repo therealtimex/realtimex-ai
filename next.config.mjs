535c0063c3b84f0cab0e27e5ce2bf0a6fed0558f
Tai Cai <taicai@rtanalytics.vn>
2025-07-11 10:43:32 +0700

    Add GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   eslint: {
     ignoreDuringBuilds: true,
   },
